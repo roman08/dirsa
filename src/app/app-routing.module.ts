ec253b715d6bbe095fa158fd87c85a2abc47bfbf
Roman Alberto Madrigal Peralta <MadrigalRA@DIRSA.DIRSAMEXICO.COM>
2023-03-28 18:06:36 -0600

    correcion rutas y actualizacion ip api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes,{useHash: true}),
     PagesRoutingModule,
     AuthRoutingModule,
   ],
